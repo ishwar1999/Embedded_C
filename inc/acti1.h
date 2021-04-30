225a28b8aa6d14c5f9fc3bede8d14c03ebcd9e4b
ISHWARKUBASAD <58268025+ishwar1999@users.noreply.github.com>
2021-04-30 18:41:12 +0530

    Update acti1.h

--- inc/acti1.h
+++ inc/acti1.h
@@ -18,25 +18,25 @@
 #include <avr/io.h>
 
 /**
- * @brief A macro to check if the button is pressed or not.
+ * @brief A macro to check if the button is pressed.
  * 
  */
 #define SENSOR !(PIND&(1<<PD0))
 
 /**
- * @brief A macro to check if the heater is pressed or not.
+ * @brief A macro to check if the heater is pressed.
  * 
  */
 #define HEATER !(PIND&(1<<PD4))
 
 /**
- * @brief A macro to turn on the LED 
+ * @brief A Macro to turn on the LED 
  * 
  */
 #define SET_LED PORTD|=(1<<PB0)
 
 /**
- * @brief A macro to turn off the LED
+ * @brief A Macro to turn off the LED
  * 
  */
 #define CLR_LED PORTD&=~(1<<PB0)
