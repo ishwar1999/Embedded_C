88f1b45816b13a52a4d4e9fd0dcc84858b3ab0af
ISHWARKUBASAD <58268025+ishwar1999@users.noreply.github.com>
2021-04-30 18:45:29 +0530

    Update projectmain.c

--- projectmain.c
+++ projectmain.c
@@ -5,11 +5,6 @@
 #include <util/delay.h>
 
 int main(void){
-
-    /**
-    * @brief A function to setup timer1 with channel A for pulse width modulation with wave generation mode of 10 bits fast PWM and prescaling of 64.
-    * 
-    */
     TCCR1A|=(1<<COM1A1)|(1<<WGM10)|(1<<WGM11);  //setting timer1 for PWM
     TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10);     //8 prescalar
     DDRB|=(1<<PB1);                             //Setting PB1 as output pin
