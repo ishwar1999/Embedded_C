4614abf45f649f0e4db8819b47035e43d452aa37
ISHWARKUBASAD <58268025+ishwar1999@users.noreply.github.com>
2021-04-30 17:56:33 +0530

    Update acti1.c

--- src/acti1.c
+++ src/acti1.c
@@ -1,1 +1,27 @@
+/**
+ * @file acti1.c
+ * @author ISHWAR KUBASAD (kubsadishwar@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+#include "acti1.h"
 
+/**
+ * @brief A function to operate the button and heater sensor
+ * 
+ */
+void Buttons_LED_Init()
+{
+     /*Configure LED and Switch pins*/
+    DDRD|=(1<<PD6);
+    DDRD&=~(1<<PD0);
+    PORTD|=(1<<PD0);
+    DDRD&=~(1<<PD4);
+    PORTD|=(1<<PD4);
+}
