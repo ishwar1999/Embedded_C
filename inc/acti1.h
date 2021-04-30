d0532333c3b318f87daa5434ef850a91de9a7d72
ISHWARKUBASAD <58268025+ishwar1999@users.noreply.github.com>
2021-04-30 17:47:44 +0530

    Update acti1.h

--- inc/acti1.h
+++ inc/acti1.h
@@ -1,1 +1,47 @@
+/**
+ * @file acti1.h
+ * @author ISHWAR KUBASAD (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+
+#ifndef _ACTI1_H
+#define _ACTI1_H
+
+#define AVR_ATmega328
+
+#include <avr/io.h>
+
+/**
+ * @brief A macro to check if the button is pressed or not.
+ * 
+ */
+#define SENSOR !(PIND&(1<<PD0))
+
+/**
+ * @brief A macro to check if the heater is pressed or not.
+ * 
+ */
+#define HEATER !(PIND&(1<<PD4))
+
+/**
+ * @brief A macro to turn on the LED 
+ * 
+ */
+#define SET_LED PORTD|=(1<<PD6)
+
+/**
+ * @brief A macro to turn off the LED
+ * 
+ */
+#define CLR_LED PORTD&=~(1<<PD6)
+
+void Buttons_LED_Init();
+
+
+#endif //ACT1_H_INCLUDED
